d234e0179abd2c4613deb1041534f384a52979d5
wooram <lovefamilydev@gmail.com>
2012-08-04 09:53:20 +0900

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MolooTemplate::Application.config.secret_token = 'db40e4732d23e3ad633ef96d331c130aa964aa40c7ff48ea82aec70f370a2da884bad08bbd6da24614c582584147ca746bfcfd5bcaf6c160dcbd9039d28f14da'
