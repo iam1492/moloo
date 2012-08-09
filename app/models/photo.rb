fc64489af7593ac14bb63c2734a60e32ec823799
kai <kai@kakao.com>
2012-08-09 13:44:52 +0900

    change

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,5 @@
 class Photo < ActiveRecord::Base
   attr_accessible :photo, :photo_url
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-
   belongs_to :product
-
 end
