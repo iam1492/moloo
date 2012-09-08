a6729ab042bb68b6390569887456b960d585ee92
kai <lovefamilydev@gmail.com>
2012-09-08 23:49:05 +0900

    fb profile

--- app/controllers/fb_sessions_controller.rb
+++ app/controllers/fb_sessions_controller.rb
@@ -4,33 +4,29 @@ class FbSessionsController < ApplicationController
 	 	@graph = Koala::Facebook::API.new(@fb_token)
 
 	 	@profile = @graph.get_object("me")
-	 	@friends = @graph.get_connections("me", "friends")
-	 	
 
-	 	logger.debug @friends.class
-
-	 	@friends_array = []
-	 	@friends.each do |friend|
-	 		@friends_array << friend['id']
-	 	end
-
-	 	logger.debug @friends_array
+	 	logger.debug @profile
 
 	 	@id = @profile['id']
 	 	@email = @profile['email']
 	 	@gender = @profile['gender']
 	 	@name = @profile['name']
 	 	@seller = false
+	 	#@profile_img_path = @graph.get_picture(@id)
+	 	
+	 	prefix = 'http://graph.facebook.com/'
+	 	postfix = '/picture'
 
+		@profile_image = open(prefix + @id + postfix)
 	 	@user = User.find_by_fb_id(@id)
 
 	 	#user not exist
 	 	if @user.nil?
 	 		password = Devise.friendly_token.first(6)
-	 		User.create!(:email => @email, :password => password,
+	 		@user = User.create!(:email => @email, :password => password,
 	 			:password_confirmation => password, :name => @name,
 	 			:gender => @gender, :fb_id => @id, :fb_access_token => @fb_token,
-	 			:seller => false)
+	 			:seller => false, :profile => @profile_image)
 	 	end
 	 	if (!@fb_token.eql?@user.fb_access_token)
 	 		User.update_attribute(:fb_access_token, @fb_token)
